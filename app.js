e799b461a2d91b089c7971efc4e22c7df7dd55a6
Oleksandr Lysakov <lysakova@yahoo.com>
2021-12-21 19:49:09 +0100

    add fetch

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 
+const buttonGetActivity = document.querySelector('.button-change-activity')
+const activityContainer = document.querySelector('.activity-container')
 
 
+const getActivity = () => {
+    fetch('https://apis.scrimba.com/bored/api/activity')
+    .then(res =>res.json())
+    .then(data => {
+        console.log(data)
+        activityContainer.textContent = data.activity
+    })
+}
+
+buttonGetActivity.addEventListener('click', getActivity)
 
 
-fetch('https://apis.scrimba.com/bored/api/activity')
-.then(response =>response.json())
-.then(data => {
-    const div = document.createElement('div')
-    div.classList.add('active')
-    div.textContent = data.activity
-    document.body.append(div)
-    console.log(data)
-})
